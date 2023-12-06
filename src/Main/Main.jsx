c854bf72ea6f9fee3896c0ddf77de910b99de550
Denis Holc <holc.denis7@gmail.com>
2023-12-06 17:13:31 +0100

    changed lottie animation size

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,7 +4,7 @@ import Cards from "../components/Cards";
 
 const Main = ({ isMenuOpen }) => {
   return (
-    <div className={isMenuOpen ? "menu-open" : ""}>
+    <div /* className={isMenuOpen ? "menu-open" : ""} */>
       <section>
         <h1 className="cities-wrapper">COUNTRIES</h1>
         <Cards />
