379dc9764e5f592aa75dd14929c50a3210886e3a
Valentijn <v.f.vermeer@gmail.com>
2023-12-10 23:45:18 +0100

    added routing

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -5,6 +5,9 @@ import { Route, Routes } from 'react-router-dom';
 import About from "../components/About"
 import Home from "../components/Home"
 import Contact from "../components/Contact"
+import CountriesOverview from "../components/CountriesOverview"
+import CitiesOverview from "../components/CitiesOverview"
+import CountryDetails from "../components/CountryDetails"
 
 const Main = ({ isMenuOpen }) => {
   return (
@@ -14,8 +17,10 @@ const Main = ({ isMenuOpen }) => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/countries" element={<Cards />} />
-        <Route path="*" element={<NotFound />} /> */}
+        <Route path="/countries" element={<CountriesOverview />} />
+        <Route path="/countries/:travelfieldscountrytitle" element={<CountryDetails />} />
+        <Route path="/cities" element={<CitiesOverview />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
         {/* <h1 className="cities-wrapper">COUNTRIES</h1>
         <Cards /> */}
