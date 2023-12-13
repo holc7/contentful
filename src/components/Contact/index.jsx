542a2e86ad5ee593deb082f0f066291215ffa219
Denis Holc <holc.denis7@gmail.com>
2023-12-13 16:44:42 +0100

    fixed contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,36 +4,39 @@ import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div className="d-flex justify-content-center">
-      <form className="d-flex flex-column justify-content-center align-items-center form-wrapper">
-        <h2>CONTACT US</h2>
+    <>
+      <div className="d-flex justify-content-center form-container">
+        <form className="d-flex flex-column justify-content-center align-items-center form-wrapper">
+          <h2>CONTACT US</h2>
 
-        <input className="form-input" type="name" placeholder="Name..." />
+          <input className="form-input" type="name" placeholder="Name..." />
 
-        <input
-          className="form-input"
-          type="email"
-          placeholder="Email address..."
-        />
+          <input
+            className="form-input"
+            type="email"
+            placeholder="Email address..."
+          />
 
-        <textarea
-          className="form-textarea"
-          name="message"
-          cols="50"
-          rows="10"
-          placeholder="Your message..."
-        ></textarea>
-        <motion.div className="button-div" whileTap={{ scale: 0.9 }}>
-          <button
-            onClick={(e) => e.preventDefault()}
-            className="send-button"
-            type="submit"
-          >
-            SEND
-          </button>
-        </motion.div>
-      </form>
-    </div>
+          <textarea
+            className="form-textarea"
+            name="message"
+            cols="50"
+            rows="10"
+            placeholder="Your message..."
+          ></textarea>
+          <motion.div className="button-div" whileTap={{ scale: 0.9 }}>
+            <button
+              onClick={(e) => e.preventDefault()}
+              className="send-button"
+              type="submit"
+            >
+              SEND
+            </button>
+          </motion.div>
+        </form>
+      </div>
+      <div className="invisible-element"></div>
+    </>
   );
 };
 
