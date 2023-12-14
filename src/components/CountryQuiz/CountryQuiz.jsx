ae04607095c3598b9b30fd2123243267cf9267a0
Denis Holc <holc.denis7@gmail.com>
2023-12-14 13:51:40 +0100

    added redirects for netlify

--- src/components/CountryQuiz/CountryQuiz.jsx
+++ src/components/CountryQuiz/CountryQuiz.jsx
@@ -1,82 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import './CountryQuiz.css';
+import React, { useState, useEffect } from "react";
+import "./CountryQuiz.css";
 
 const quizQuestions = [
   // ... (your quiz questions)
   {
     question: "What is the capital of France?",
     options: ["Paris", "Berlin", "Rome"],
-    answer: "Paris"
+    answer: "Paris",
   },
   {
     question: "What is the capital of Pakistan?",
     options: ["Islamabad", "Dehli", "Lahore"],
-    answer: "Islamabad"
+    answer: "Islamabad",
   },
 
-{
-  question: "What is the capital of the USA?",
-  options: ["Washington D.C.", "New York", "Los Angeles"],
-  answer: "Washington D.C."
-},
-{
-  question: "What is the capital of the Netherlands?",
-  options: ["Amsterdam", "Rotterdam", "The Hague"],
-  answer: "Amsterdam"
-},
-{
-  question: "What is the capital of Spain?",
-  options: ["Madrid", "Barcelona", "Valencia"],
-  answer: "Madrid"
-},
-{
-  question: "What is the capital of Turkey?",
-  options: ["Ankara", "Istanbul", "Izmir"],
-  answer: "Ankara"
-},
-{
-  question: "What is the capital of Portugal?",
-  options: ["Lisbon", "Porto", "Faro"],
-  answer: "Lisbon"
-},
-{
-  question: "What is the capital of Slovenia?",
-  options: ["Ljubljana", "Maribor", "Koper"],
-  answer: "Ljubljana"
-},
-{
-  question: "What is the capital of Colombia?",
-  options: ["Bogotá", "Medellín", "Cali"],
-  answer: "Bogotá"
-}
-
+  {
+    question: "What is the capital of the USA?",
+    options: ["Washington D.C.", "New York", "Los Angeles"],
+    answer: "Washington D.C.",
+  },
+  {
+    question: "What is the capital of the Netherlands?",
+    options: ["Amsterdam", "Rotterdam", "The Hague"],
+    answer: "Amsterdam",
+  },
+  {
+    question: "What is the capital of Spain?",
+    options: ["Madrid", "Barcelona", "Valencia"],
+    answer: "Madrid",
+  },
+  {
+    question: "What is the capital of Turkey?",
+    options: ["Ankara", "Istanbul", "Izmir"],
+    answer: "Ankara",
+  },
+  {
+    question: "What is the capital of Portugal?",
+    options: ["Lisbon", "Porto", "Faro"],
+    answer: "Lisbon",
+  },
+  {
+    question: "What is the capital of Slovenia?",
+    options: ["Ljubljana", "Maribor", "Koper"],
+    answer: "Ljubljana",
+  },
+  {
+    question: "What is the capital of Colombia?",
+    options: ["Bogotá", "Medellín", "Cali"],
+    answer: "Bogotá",
+  },
 ];
 const shuffleArray = (array) => {
-  let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
   return array;
 };
 
 const CountryQuiz = () => {
-  const [shuffledQuestions, setShuffledQuestions] = useState(() => shuffleArray([...quizQuestions]));
+  const [shuffledQuestions, setShuffledQuestions] = useState(() =>
+    shuffleArray([...quizQuestions])
+  );
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(() => {
-    return parseInt(localStorage.getItem('currentQuestionIndex') || 0, 10);
+    return parseInt(localStorage.getItem("currentQuestionIndex") || 0, 10);
   });
-  const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
   const [score, setScore] = useState(() => {
-    return parseInt(localStorage.getItem('score') || 0, 10);
+    return parseInt(localStorage.getItem("score") || 0, 10);
   });
-  const [feedback, setFeedback] = useState('');
+  const [feedback, setFeedback] = useState("");
   const [timeoutId, setTimeoutId] = useState(null);
 
   useEffect(() => {
-    localStorage.setItem('currentQuestionIndex', currentQuestionIndex.toString());
-    localStorage.setItem('score', score.toString());
+    localStorage.setItem(
+      "currentQuestionIndex",
+      currentQuestionIndex.toString()
+    );
+    localStorage.setItem("score", score.toString());
 
     return () => {
       if (timeoutId) clearTimeout(timeoutId);
@@ -84,7 +91,7 @@ const CountryQuiz = () => {
   }, [currentQuestionIndex, score, timeoutId]);
 
   const handleOptionClick = (option) => {
-    if (feedback === '') {
+    if (feedback === "") {
       setSelectedOption(option);
     }
   };
@@ -92,29 +99,32 @@ const CountryQuiz = () => {
   const handlePreviousQuestion = () => {
     if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
-      setFeedback('');
+      setFeedback("");
     }
   };
 
   const handleNextQuestion = () => {
-    if (feedback === '') {
+    if (feedback === "") {
       if (selectedOption === shuffledQuestions[currentQuestionIndex].answer) {
-        setScore(prevScore => prevScore + 1);
-        setFeedback('Correct!');
+        setScore((prevScore) => prevScore + 1);
+        setFeedback("Correct!");
       } else {
-        setFeedback('Incorrect. The correct answer was: ' + shuffledQuestions[currentQuestionIndex].answer);
+        setFeedback(
+          "Incorrect. The correct answer was: " +
+            shuffledQuestions[currentQuestionIndex].answer
+        );
       }
 
       const newTimeoutId = setTimeout(() => {
-        setFeedback('');
-        setSelectedOption('');
+        setFeedback("");
+        setSelectedOption("");
         if (currentQuestionIndex < shuffledQuestions.length - 1) {
           setCurrentQuestionIndex(currentQuestionIndex + 1);
         } else {
           setCurrentQuestionIndex(0);
           setScore(0);
-          localStorage.removeItem('currentQuestionIndex');
-          localStorage.removeItem('score');
+          localStorage.removeItem("currentQuestionIndex");
+          localStorage.removeItem("score");
         }
       }, 2000);
 
@@ -122,25 +132,52 @@ const CountryQuiz = () => {
     }
   };
 
-  const progress = ((currentQuestionIndex + 1) / shuffledQuestions.length) * 100;
+  const progress =
+    ((currentQuestionIndex + 1) / shuffledQuestions.length) * 100;
   const scorePercentage = (score / shuffledQuestions.length) * 100;
 
   return (
-    <div>
+    <div className="quiz-container">
       <div className="progress">
-        <div className="progress-bar" role="progressbar" style={{ width: `${progress}%` }} aria-valuenow={progress} aria-valuemin="0" aria-valuemax="100"></div>
+        <div
+          className="progress-bar"
+          role="progressbar"
+          style={{ width: `${progress}%` }}
+          aria-valuenow={progress}
+          aria-valuemin="0"
+          aria-valuemax="100"
+        ></div>
       </div>
       {currentQuestionIndex < shuffledQuestions.length ? (
         <>
           <h2>{shuffledQuestions[currentQuestionIndex].question}</h2>
-          {shuffledQuestions[currentQuestionIndex].options.map((option) => (
-            <button key={option} onClick={() => handleOptionClick(option)} disabled={feedback !== '' || selectedOption === option}>
-              {option}
+          <div className="answer-button">
+            {shuffledQuestions[currentQuestionIndex].options.map((option) => (
+              <button
+                className="quiz-button"
+                key={option}
+                onClick={() => handleOptionClick(option)}
+                disabled={feedback !== "" || selectedOption === option}
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+          <div className="prev-next-button">
+            <button
+              className="quiz-button"
+              onClick={handlePreviousQuestion}
+              disabled={currentQuestionIndex === 0}
+            >
+              Previous
             </button>
-          ))}
-          <div>
-            <button onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>Previous</button>
-            <button onClick={handleNextQuestion} disabled={selectedOption === ''}>Next</button>
+            <button
+              className="quiz-button"
+              onClick={handleNextQuestion}
+              disabled={selectedOption === ""}
+            >
+              Next
+            </button>
           </div>
         </>
       ) : (
