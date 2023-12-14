c35d4251f7c77333f7f133687c4568a9d26b439c
Denis Holc <holc.denis7@gmail.com>
2023-12-14 12:36:40 +0100

    added redirects for netlify

--- src/components/CountryDetails/index.jsx
+++ src/components/CountryDetails/index.jsx
@@ -14,9 +14,9 @@ import { Marker } from "react-leaflet/Marker";
 import { Popup } from "react-leaflet/Popup";
 import CountUp from "react-countup";
 import CommentSection from "../CommentSection/CommentSection";
-import '../../components/CommentSection/CommentSection'
+import "../../components/CommentSection/CommentSection";
 import CountryQuiz from "../CountryQuiz/CountryQuiz";
-import '../../components/CountryQuiz/CountryQuiz'
+import "../../components/CountryQuiz/CountryQuiz";
 
 const CountryDetails = () => {
   const { entryid } = useParams();
@@ -178,9 +178,11 @@ const CountryDetails = () => {
         <h2 className="quiz-title">Country Quiz</h2>
         <p>Now its time to Test your knowledge about countries!</p>
       </div>
-            <CountryQuiz />
+      <CountryQuiz />
 
-<CommentSection countryName={countryData?.fields?.countrytitle || 'Unknown Country'} />
+      <CommentSection
+        countryName={countryData?.fields?.countrytitle || "Unknown Country"}
+      />
       <motion.button
         whileTap={{ scale: 0.9 }}
         className="read-more-button countrydetails-read-more-button"
